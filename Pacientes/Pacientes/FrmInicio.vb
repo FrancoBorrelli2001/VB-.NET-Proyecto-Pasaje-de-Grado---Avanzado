744c9aa4b6bd74015cb852680e90f5a9b265b203
HelloCodeSoftware <66492117+HelloCodeSoftware@users.noreply.github.com>
2020-11-06 14:15:32 -0300

    Hotfix

--- Pacientes/Pacientes/FrmInicio.vb
+++ Pacientes/Pacientes/FrmInicio.vb
@@ -5,7 +5,6 @@ Public Class FrmInicio
     Dim appPacientes As FrmLogin
     Private Sub FrmInicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         appPacientes = New FrmLogin
-        InicializarConfig(TiposUsuario.Paciente)
     End Sub
 
     Private Sub tmrSplashWindow_Tick(sender As Object, e As EventArgs) Handles tmrSplashWindow.Tick
