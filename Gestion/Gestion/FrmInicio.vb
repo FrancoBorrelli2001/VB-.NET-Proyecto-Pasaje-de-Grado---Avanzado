ea047fbcc42dd748a055de7ba8dc8a00d146e19b
HelloCodeSoftware <66492117+HelloCodeSoftware@users.noreply.github.com>
2020-11-06 14:14:05 -0300

    Hotfix

--- Gestion/Gestion/FrmInicio.vb
+++ Gestion/Gestion/FrmInicio.vb
@@ -5,7 +5,6 @@ Public Class FrmInicio
     Dim appGestion As FrmLogin
     Private Sub FrmInicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         appGestion = New FrmLogin
-        InicializarConfig(TiposUsuario.Administrativo)
     End Sub
 
     Private Sub tmrSplashWindow_Tick(sender As Object, e As EventArgs) Handles tmrSplashWindow.Tick
