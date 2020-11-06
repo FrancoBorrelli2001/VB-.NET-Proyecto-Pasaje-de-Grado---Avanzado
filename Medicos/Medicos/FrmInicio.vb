8ce03565e1b7c62a655ae101065f7b7d34f52a37
HelloCodeSoftware <66492117+HelloCodeSoftware@users.noreply.github.com>
2020-11-06 14:14:51 -0300

    Hotifx

--- Medicos/Medicos/FrmInicio.vb
+++ Medicos/Medicos/FrmInicio.vb
@@ -5,7 +5,6 @@ Public Class FrmInicio
     Dim frmLogin As FrmLogin
     Private Sub FrmInicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         frmLogin = New FrmLogin
-        InicializarConfig(TiposUsuario.Medico)
     End Sub
 
     Private Sub tmrSplashWindow_Tick(sender As Object, e As EventArgs) Handles tmrSplashWindow.Tick
